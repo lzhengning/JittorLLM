5a13129150964481e33998da5b2cb89fe361b260
Exusial <phy18cs@gmail.com>
2023-04-02 00:30:49 +0800

    add web for chatglm.

--- models/chatglm/__init__.py
+++ models/chatglm/__init__.py
@@ -9,14 +9,21 @@ class ChatGLMMdoel(LLMModel):
         self.model = AutoModel.from_pretrained(os.path.dirname(__file__), trust_remote_code=True).half().cuda()
         self.model = self.model.eval()
 
+    def run(self, text, history=[]):
+        return self.model.chat(self.tokenizer, text, history=history)
+    
     def chat(self) -> str:
         history = []
         while True:
             text = input("用户输入:")
-            response, history = self.model.chat(self.tokenizer, text, history=history)
+            response, history = self.run(text, history=history)
             print("Chat-GLM: ", end='')
             print(response)
             print("")
+    
+    def run_web_demo(self, input_text, history=[]):
+        while True:
+            yield self.run(input_text, history=history)
 
 def get_model(args):
     return ChatGLMMdoel(args)
